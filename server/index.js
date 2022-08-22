b5ef92ad2998c058d1c662543f5862081663331c
Dmitrii Nikolskii <dmnikolskii@gmail.com>
2022-08-22 23:47:23 +0600

    package change 1

--- server/index.js
+++ server/index.js
@@ -18,6 +18,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.listen();
+
+//  app.listen(PORT, () => {
+//    console.log(`Server listening on ${PORT}`);
+//  });
